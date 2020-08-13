b02ad73e1c68f0a0a983121e2e741fc6000e6791
inmove <lisper.inmove@gmail.com>
2020-08-13 13:28:50 +0800

    done

---
+++ AESUtil.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import time
+import base64
+from Crypto.Cipher import AES
+
+
+class Prpcrypt():
+    def __init__(self):
+        # key值（密码）
+        self.key = '****************'.encode("utf-8")
+        # vi偏移量
+        self.iv = '****************'.encode("utf-8")
+        self.mode = AES.MODE_CBC
+        self.BS = AES.block_size
+        self.pad = lambda s: s + (self.BS - len(s) % self.BS) * chr(self.BS - len(s) % self.BS)
+        self.unpad = lambda s: s[0:-ord(s[-1])]
+
+    def encrypt(self, text):
+        text = self.pad(text).encode("utf-8")
+        cryptor = AES.new(self.key, self.mode, self.iv)
+        ciphertext = cryptor.encrypt(text)
+        return base64.b64encode(bytes(ciphertext))
+
+    def decrypt(self, text):
+        text = base64.b64decode(text)
+        cryptor = AES.new(self.key, self.mode, self.iv)
+        plain_text = cryptor.decrypt(text)
+        return self.unpad(bytes.decode(plain_text).rstrip('\0'))    # 解密字节？？？
+
+    def padding_to_16(self, value):
+        pass
+
+
+if __name__ == '__main__':
+    pc = Prpcrypt()  # 初始化密钥 和 iv
+    # text='access&a494fcbc-9aa1-4718-bd7d-a90d01211d97&0&2&'
+    text = 'access&a494fcbc-9aa1-4718-bd7d-a90d01211d97&0&1&'
+    # text='access&a494fcbc-9aa1-4718-bd7d-a90d01211d97&1&1&'
+    # text='access&a494fcbc-9aa1-4718-bd7d-a90d01211d97&1&2&'
+    # text='update&a494fcbc-9aa1-4718-bd7d-a90d01211d97&0&2&'
+    # text='update&a494fcbc-9aa1-4718-bd7d-a90d01211d98&0&2&'
+    # text='logout&'
+    e = pc.encrypt(text + str(int(time.time() / 10)))  # 加密
+    d = pc.decrypt(e)  # 解密
+    print("加密:%s" % e.decode("utf-8"))
+    print("解密:%s"% d)
+    print("长度:%s"% len(d))
--- map.py
+++ map.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class Map(object):
 
     def __init__(self, **kargs):
@@ -11,33 +12,10 @@ class Map(object):
         self.data[k] = v
 
     def __getattr__(self, k):
-        ret = self.data.get(k, None)
-        if not ret:
-            return None
-        if isinstance(ret, int):
-            return ret
-        if isinstance(ret, str):
-            return ret
-        if isinstance(ret, list):
-            for index, r in enumerate(ret):
-                if isinstance(r, dict):
-                    r = Map(**r)
-                    ret[index] = r
-            return ret
-        if isinstance(ret, dict):
-            r = Map(**ret)
-            self.data[k] = r
-            return r
-        return ret
-
-
-class ClassA:
-    def __init__(self):
-        self.name = "inmove"
+        return self.data.get(k)
 
 
 if __name__ == '__main__':
     obj = Map()
     obj.name = "inmove"
-    obj.user = {"name": "inmove", "age": 30, "job": {"address": 'shenZhen', "content": "programmer", "obj": ClassA()}}
-    print(obj.user.job.obj)
+    print(obj.name)
